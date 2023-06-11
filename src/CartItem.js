70598aa0d22b7adefc25c63793befbea64c2d245
gknanhe <cse18gknanhejd@gmail.com>
2023-06-11 10:28:59 +0530

    add increaseQuantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,19 +1,56 @@
 import React from "react";
 
 
+
 class CartItem extends React.Component {
+    constructor(){
+        super(); // to call the constructor of inherited class (React. Componant)
+        this.state = {
+            price: 999,
+            title: "Mobile Phone",
+            qty: 1,
+            img: ''
+        }
+        // this.increaseQuantity = this.increaseQuantity.bind(this)   //can also work but messi
+       
+    }
+
+    //Arrow functions asig this (.bind(this)) 
+    increaseQuantity= () => {
+        console.log('this.state',this.state);
+    }
+
+    
     render (){
+        const { price, title, qty } = this.state;   //object destucturing
         return(
             <div className="cart-item">
                 <div className="left-block"> 
-                    <img style={styles.image} alt="img" />
+                    <img style={styles.image} 
+                    alt="img" />
+ 
                 </div>
+   
                <div className="right-block"> 
-                    <div style={{fontSize: 25}}>Phone</div>
-                    <div style={{color: '#777'}}>Rs 999</div>
-                    <div style={{color: '#777'}}>Qty: 1</div>
+                    <div style={{fontSize: 25}}>{title}</div>   {/* can also be used {this.state.title} */}
+                    <div style={{color: '#777'}}>Rs {price}</div>
+                    <div style={{color: '#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img 
+                            alt="increase"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/512/1828/1828926.png"
+                            onClick={this.increaseQuantity}   
+                            />   {/* Can also write like this -> this.increaseQuantity.bind(this) */}
+                        <img 
+                            alt="decrease"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png"/>
+                        <img 
+                            alt="delete" 
+                            className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/128/3385/3385559.png"/>
                     </div>
                 </div> 
             </div>
