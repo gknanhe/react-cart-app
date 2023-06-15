2c73960f1d5c3acab6066bcdedee7d7d3472a7fe
gknanhe <cse18gknanhejd@gmail.com>
2023-06-15 19:18:35 +0530

    add total price

--- src/App.js
+++ src/App.js
@@ -13,21 +13,21 @@ class App extends React.Component {
             price: 999,
             title: "Mobile Phone",
             qty: 10,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1546054454-aa26e2b734c7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bW9iaWxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
             id: 1
           },
           {
             price: 99,
             title: "Watch",
             qty: 1,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0Y2h8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
             id: 2
           },
           {
             price: 9999,
             title: "Laptop",
             qty: 4,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwZm9yJTIwc2FsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
             id: 3
           },
         ]
@@ -89,6 +89,17 @@ class App extends React.Component {
       return count;
     }
 
+    getTotalPrice = ()=>{
+      const {products} = this.state;
+
+      let cartTotal =0;
+
+      products.forEach((product)=>{
+        cartTotal = cartTotal + product.qty * product.price;
+      });
+      return cartTotal;
+    }
+
   render(){
     const { products } = this.state;
 
@@ -101,6 +112,7 @@ class App extends React.Component {
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
         />
+        <div style={{fontSize: 20 , padding: 10}}>Total: {this.getTotalPrice()}</div>
       </div>
     );
   }
--- src/CartItem.js
+++ src/CartItem.js
@@ -56,7 +56,8 @@ const CartItem = (props) => { //PROPS added by Default
         return(
             <div className="cart-item">
                 <div className="left-block"> 
-                    <img style={styles.image} 
+                    <img style={styles.image}
+                    src={product.img} 
                     alt="img" />
  
                 </div>
